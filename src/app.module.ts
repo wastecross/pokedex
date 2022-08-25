91e9d115124016623167de1c73a3be98b88cbb3f
Charly MI <charly_1310@outlook.com>
2022-08-25 14:12:08 -0500

    feat: add serve static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+  ],
   controllers: [],
   providers: [],
 })
