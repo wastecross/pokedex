0d9b73abd2ef2e0a7d34ac5fc9280939559bbbe5
Charly MI <charly_1310@outlook.com>
2022-08-25 17:27:15 -0500

    feat: add mongoose conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -6,6 +7,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
   controllers: [],
