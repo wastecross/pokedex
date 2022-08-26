61d3363dad6d62a67e9d5833af3d88f43ab53811
Charly MI <charly_1310@outlook.com>
2022-08-26 00:42:44 -0500

    feat(app.module.ts): import common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
