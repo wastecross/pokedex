e86108f09766f097c0e5c88ceb2a664b5f0870cf
Charly MI <charly_1310@outlook.com>
2022-08-29 14:28:06 -0500

    feat(seed.service.ts): execute pokemon service to create pokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonMode: Model<Pokemon>,
+    private readonly pokemonService: PokemonService,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=151',
     );
 
-    data.results.forEach(({ name, url }) => {
+    const promises = data.results.map(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      console.log(name, no);
+      await this.pokemonService.create({ name, no });
     });
+
+    await Promise.all(promises);
 
     return data.results;
   }
