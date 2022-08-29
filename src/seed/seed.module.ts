97ba9ff4b02ded88c03dcfc3753f51740e887523
Charly MI <charly_1310@outlook.com>
2022-08-29 14:27:48 -0500

    feat(seed.module.ts): add model and service

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService],
+  providers: [SeedService, PokemonService],
+  imports: [
+    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonSchema }]),
+  ],
 })
 export class SeedModule {}
