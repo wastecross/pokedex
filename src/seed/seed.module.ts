6e5191ed5d166a6f0d7f9d8b986b09085f6a0c1b
Charly MI <charly_1310@outlook.com>
2022-08-29 14:38:14 -0500

    feat(seed.module.ts): simplify import

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
-import { PokemonService } from 'src/pokemon/pokemon.service';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService, PokemonService],
-  imports: [
-    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonSchema }]),
-  ],
+  providers: [SeedService],
+  imports: [PokemonModule],
 })
 export class SeedModule {}
