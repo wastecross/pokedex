fb91a492332f6818b8a2c0de585f11bf9e4394c7
Charly MI <charly_1310@outlook.com>
2022-08-29 18:00:43 -0500

    feat(main.ts): add configuration to transform dto

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
   );
 
   await app.listen(3000);
