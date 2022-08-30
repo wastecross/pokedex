311badc35cb16051366494d9619694ba817a70cb
Charly MI <charly_1310@outlook.com>
2022-08-29 21:52:38 -0500

    feat(app.module.ts): import env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,13 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ load: [EnvConfiguration] }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule,
