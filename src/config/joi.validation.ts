0bc69d035ddd56807c99214075502f6fa23e78ba
Charly MI <charly_1310@outlook.com>
2022-08-30 13:31:42 -0500

    feat(config): remove port

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,5 @@ import * as joi from 'joi';
 
 export const JoiValidationSchema = joi.object({
   MONGODB: joi.required(),
-  PORT: joi.number().default(3001),
   DEFAULT_LIMIT: joi.number().default(7),
 });
