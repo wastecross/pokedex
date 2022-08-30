76587db9386d4ce92baf0a296ef7130be7a50d08
Charly MI <charly_1310@outlook.com>
2022-08-30 12:52:41 -0500

    fix(seed): import correctly

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { PokemonModule } from 'src/pokemon/pokemon.module';
-import { CommonModule } from 'src/common/common.module';
+import { PokemonModule } from '../pokemon/pokemon.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   controllers: [SeedController],
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
